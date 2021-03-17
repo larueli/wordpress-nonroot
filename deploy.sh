b1caf7dd33082b6e6c23590e2d5f03f699bbf6a8
Ivann LARUELLE <37900449+larueli@users.noreply.github.com>
2021-03-18 00:02:47 +0100

    fix mv

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@ if [ ! -f /var/www/html/wp-config-sample.php ]; then
     cd /tmp
     wget https://wordpress.org/wordpress-${WORDPRESS_VERSION}.zip
     unzip wordpress-${WORDPRESS_VERSION}.zip
-    mv -rf wordpress/* /var/www/html/
+    mv -f wordpress/* /var/www/html/
     rm wordpress-${WORDPRESS_VERSION}.zip
 fi
