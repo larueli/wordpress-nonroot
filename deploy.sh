e3ed894fcd1eb3e3458d3a4170e4b68759cc3113
Ivann LARUELLE <ivann+git@laruelle.me>
2021-04-30 10:45:27 +0200

    better deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ ! $(ls -A /var/www/html) ]; then
+if [ ! -f /var/www/html/index.php ]; then
     cd /tmp
     unzip wordpress-${WORDPRESS_VERSION}.zip
     mv -f wordpress/* /var/www/html/
